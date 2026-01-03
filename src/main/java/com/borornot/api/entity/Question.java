6b52716d549a3c64c3809808645029331db1ce91
wujuno <huj88854@gmail.com>
2026-01-03 19:54:56 +0900

    Feat: Question entity 추가
    - 질문 데이터를 위한 JPA 엔티티 정의
    - 엔티티 스캔을 위한 패키지 구조 재배치 및 자동 테이블 생성 확인

---
+++ src/main/java/com/borornot/api/entity/Question.java
@@ -0,0 +1,21 @@
+package com.borornot.api.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "tb_questions") // DB에 생성될 테이블 이름
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content; // 질문 내용
+
+    private String category; // 질문 카테고리
+}
